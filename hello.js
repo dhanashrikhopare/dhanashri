b7b13c20290de6c25823deb33ec88ecec5e205f7
dhanashrikhopare <dhanashrikhopare@gmail.com>
2025-06-05 09:27:43 +0530

    starter code

---
+++ hello.js
@@ -0,0 +1,20 @@
+console.log("hello");
+
+let val=19;
+val='hello';
+val=false
+val='world'
+console.log(val)
+
+const pi=3.15;
+pi=3.14
+
+console.log(pi)
+
+let fil= 10;
+if(fil %2==0){
+    console.log("fill is even")
+}
+else{
+    console.log("fill is odd")
+}
